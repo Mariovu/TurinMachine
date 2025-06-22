475996e751c454ec9e02461bd9608a137b869308
Mariovu <mario._vulor@hotmail.com>
2025-06-22 10:20:42 -0600

    MachineTurin
    Compila

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,4 @@
 module com.example.turinmachine {
-    requires javafx.controls;
-    requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-
-    opens com.example.turinmachine to javafx.fxml;
     exports com.example.turinmachine;
 }
